c809c0b2f1dafc3b666e979e9951e0747193d055
Cheryl L <cheryl.loeffler@gmail.com>
2023-07-17 12:24:22 -0600

    routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes.js');
+const loginRoutes = require('./loginRoutes.js');
+const signupRoutes = require('./signupRoutes.js');
 
-router.use('/login', userRoutes);
-router.use('/signup', userRoutes);
+router.use('/login', loginRoutes);
+router.use('/signup', signupRoutes);
 router.use('/dashboard', postRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,20 +19,24 @@ router.get ('/', async (req, res) => {
         
         res.render('home', {
             posts,
-            //logged_in: req.session.logged_in,
+            logged_in: req.session.logged_in,
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-
 // GET login page
-
-router.get('/login', (req, res) => {
-   res.redirect('/login');
-   res.render('login');
-});
+// router.get('/login', (req, res) => {
+//     try {
+//         if (req.session.logged_in) {
+//         res.redirect('/');
+//     }
+//     res.redirect('/api/login');
+//     res.render('login');
+//     } catch (err) {
+//     res.status(500).json(err);
+// }});
         
 
 module.exports = router;
